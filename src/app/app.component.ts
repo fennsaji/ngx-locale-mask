3ab7acc8f8c7dde364bac5a346f1a62cc7c3647b
fennsaji <fennig1997@gmail.com>
2018-09-01 15:11:29 +0530

    demo model ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import locale from '@angular/common/locales/en-DE';
+import localeEU from '@angular/common/locales/en-DE';
 import localeIN from '@angular/common/locales/en-IN';
+import localeAU from '@angular/common/locales/en-AU';
 
 import { CurrencyMask } from 'ngx-locale-mask';
 import { registerLocaleData, formatCurrency } from '@angular/common';
@@ -13,17 +14,30 @@ import { registerLocaleData, formatCurrency } from '@angular/common';
 
 export class AppComponent {
   title = 'ngx-locale-mask-app';
-  locale = locale;
+  locale = localeIN;
   maskOptions: CurrencyMask;
+  localeArr = ['en-IN', 'en-DE', 'en-AU'];
 
   _val;
   set val(val) { this._val = val; console.log(val) };
   get val() { return this._val }
 
   constructor() {
-    registerLocaleData(locale)
-    this.maskOptions = { localeName: 'en-DE', currency: '₹', currencyCode: '$', digitsInfo: '3.0-4' };
-    document.write(formatCurrency(1233.545, 'en-IN', 'EUR','EUR',''))
+    this.maskOptions = { localeName: 'en-IN', currency: '₹', currencyCode: '$', digitsInfo: '2.1-4' };
+    registerLocaleData(this.locale) 
+  }
 
+  changeLocale(data) {
+    if(data == 'en-DE') {
+      this.maskOptions = { localeName: 'en-DE', currency: 'EUR', currencyCode: '$', digitsInfo: '3.0-4' };
+      this.locale = localeEU;
+    } else if(data == 'en-IN') {
+      this.maskOptions = { localeName: 'en-IN', currency: '₹', currencyCode: '$', digitsInfo: '2.1-4' };
+      this.locale = localeIN;
+    } else {
+      this.maskOptions = { localeName: 'en-AU', currency: '$', currencyCode: '$', digitsInfo: '1.0-4' };
+      this.locale = localeAU;
+    }
+    registerLocaleData(this.locale) 
   }
 }
